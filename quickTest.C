3d576043f20b4a8481f2388910ae0c7fa66696ff
nucleosynthesis <n.c.k.wardle@googlemail.com>
2015-03-04 21:34:00 +0100

    Tests aren't working

--- quickTest.C
+++ quickTest.C
@@ -1,5 +1,19 @@
 #include "RooStats/BayesianCalculator.h"
 #include "RooUniform.h"
+#include "RooRealVar.h"
+#include "RooFormulaVar.h"
+#include "RooAbsPdf.h"
+#include "RooProdPdf.h"
+#include "RooAddPdf.h"
+#include "RooWorkspace.h"
+#include "RooDataHist.h"
+#include "RooDataSet.h"
+#include "RooGaussian.h"
+#include "TFile.h"
+#include "TROOT.h"
+#include "RooPlot.h"
+#include "TCanvas.h"
+
 using namespace RooStats;
 void quickTest(){
 
@@ -7,12 +21,12 @@ void quickTest(){
   gROOT->ProcessLine(".x paperStyle.C");
 
   TFile *fi = TFile::Open("envelopews_wsignal_toy1_110to150.root");
-  RooWorkspace *multipdf = fi->Get("multipdf");
+  RooWorkspace *multipdf = (RooWorkspace*)fi->Get("multipdf");
   RooRealVar *x    = multipdf->var("CMS_hgg_mass");
 
   // The data we will use (a Toy dataset);
-  RooDataHist *datatoy = multipdf->data("roohist_data_mass_cat1_toy1__CMS_hgg_mass");
-
+  RooDataHist *datatoy = (RooDataHist *)multipdf->data("roohist_data_mass_cat1_toy1_cutrange__CMS_hgg_mass");
+  
   // Build signal model
   RooRealVar mean("mean","mean",125); mean.setConstant();
   RooRealVar sigma("sigma","sigma",1.19); sigma.setConstant();
@@ -21,35 +35,70 @@ void quickTest(){
   RooGaussian sig_pdf("gaus","gaus",*x,mean,sigma);
   
   // Should add this to the RooWorkspace
-  RooRealVar nbkg("nbkg","nbkg",datatoy->sumEntries(),0,10E6);
+  RooRealVar nbkg("nbkg","nbkg",datatoy->sumEntries(),0,1000);
 
-  RooRealVar mu("r","r",1,-10,10);
+  RooRealVar mu("r","r",1,0,5);
   RooFormulaVar nsig("nsig","nsig","@0*@1",RooArgList(mu,nsignal_const));
-  mu.setConstant();
+  //mu.setConstant();
 
  // start wih a model !
   RooAbsPdf *bkg_pdf = multipdf->pdf("env_pdf_1_8TeV_pow1");
   RooAddPdf spdf_a("Asplusb","Asplusb",RooArgList(sig_pdf,*bkg_pdf),RooArgList(nsig,nbkg));
+  //RooAddPdf spdf("Asplusb","Asplusb",RooArgList(sig_pdf,*bkg_pdf),RooArgList(nsig,nbkg));
 
-  multipdf->import(spdf,RooFit::RecycleConflictNodes());
 
   // make priors
-  multipdf->factory("Uniform::prior_mu(mu,0,10)");
-  multipdf->factory("Uniform::prior_nbkg(nbkg,0,10000)");
-  multipdf->factory("Uniform::prior_par(env_pdf_1_8TeV_pow1_p1,-10,1)");
-  multipdf->factory("PROD::priorpdf(prior_nbkg,prior_par,prior_mu)");
+ // multipdf->factory("Uniform::prior_mu(mu,0,10)");
+ // multipdf->factory("Uniform::prior_nbkg(nbkg,0,10000)");
+ // multipdf->factory("Uniform::prior_par(env_pdf_1_8TeV_pow1_p1,-10,1)");
+//  multipdf->factory("Uniform::prior_mu");
+//  multipdf->factory("Uniform::prior_nbkg");
+//  multipdf->factory("Uniform::prior_par");
+  RooUniform uniform_nbkg("p_nbkg","p_nbkg",nbkg);
+  RooUniform uniform_par("p_par","p_nbkg",*(multipdf->var("env_pdf_1_8TeV_pow1_p1")));
+  RooUniform uniform_mu("p_mu","p_nbkg",mu);
+  multipdf->var("env_pdf_1_8TeV_pow1_p1")->setMin(-6);
+  multipdf->var("env_pdf_1_8TeV_pow1_p1")->setMax(-2);
+  multipdf->Print();
 
-  RooProdPdf spdf("splusb","splusb",RooArgList(spdf_a,*multipdf->pdf("priorpdf")));
+  //multipdf->factory("PROD::priorpdf(prior_nbkg,prior_par)");
+  RooProdPdf prior_pdf("prior_nuis","nuisance prior",RooArgList(uniform_nbkg,uniform_par));
+  //RooProdPdf prior_pdf("prior_nuis","nuisance prior",RooArgList(uniform_nbkg));
 
+  RooProdPdf spdf("splusb","splusb",RooArgList(spdf_a,prior_pdf));
+  //multipdf->import(spdf,RooFit::RecycleConflictNodes());
+  spdf_a.fitTo(*datatoy);
+  RooPlot *pl1 = x->frame();
+  datatoy->plotOn(pl1);
+  spdf_a.plotOn(pl1);
+   pl1->Draw();
+
+  TCanvas *can = new TCanvas();
   // Nuisance parameters will be background parameters
   RooArgSet nuisances(*(bkg_pdf->getParameters(*datatoy)));
   nuisances.add(nbkg);
   nuisances.Print("V");
 
-  multipdf->pdf("priorpdf")->Print();
-  RooStats::BayesianCalculator bc(*datatoy,spdf,RooArgSet(mu),(*multipdf->pdf("priorpdf")),RooArgSet(nuisances));
-  RooAbsReal * posterior = bc.GetPosteriorFunction();
-  RooPlot *pl = x->frame();
-  posterior->plotOn(pl);
-  pl->Draw();
+//  multipdf->pdf("prior_mu")->Print();
+ datatoy->Print();
+spdf.Print();
+mu.Print();
+uniform_mu.Print();
+
+  RooArgSet poi(mu);
+
+  RooStats::ModelConfig mc("mc","mc",multipdf);
+  mc.SetPdf(spdf);
+  mc.SetPriorPdf(uniform_mu);
+
+  //RooStats::BayesianCalculator bc(*datatoy,spdf,poi,uniform_mu,(&nuisances));
+  //bc.Print("v");
+  //bc.SetConfidenceLevel(0.683);
+  //RooAbsPdf * posterior = bc.GetPosteriorPdf();
+  //RooPlot *pl =  bc.GetPosteriorPlot() ;//mu.frame();
+//  posterior->plotOn(pl);
+  RooStats::MCMCCalculator MCMC(*data,mc);
+  MCMC.
+
+ pl->Draw();
 }
